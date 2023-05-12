45e1ed09fdd59a6c2af369cb556d12ff1e7ecc41
sefa kapısız <sefa.kapisiz@coino.com>
2023-05-12 08:43:21 +0300

    upd

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 
-const dic: { [key: String]: String } = {};
+const dic = {};
 dic['('] = ')';
 dic[')'] = '(';
 dic['['] = ']';
@@ -14,8 +14,12 @@ dic['}'] = '{';
 var isValid = function (s) {
     for (let i = 0; i < s.length; i += 2) {
         if (s.includes(s[i])) {
-            if (s)
+            if (s.indexOf(s[i]) !== (s.length - s.indexOf(dic[s[i]])))
+                return false
         }
+
+        //both of them could not be true either this or that
+
         else if (s[i] !== dic[s[i + 1]])
             return false;
     }
