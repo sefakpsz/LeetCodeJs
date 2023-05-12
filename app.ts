22af36fdc7d6450b631a7f9439b209054fc19301
sefa kapısız <sefa.kapisiz@coino.com>
2023-05-12 08:24:20 +0300

    upd

--- app.ts
+++ app.ts
@@ -1,59 +1,25 @@
-const findMinSizeWord = (myWords) => {
-    let minLenght = myWords[0].trim().length
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 
-    for (let word of myWords) {
-        let currentWordLenght = word.trim().length;
-        if (currentWordLenght < minLenght)
-            minLenght = currentWordLenght
-    }
+const dic = {};
+dic['('] = ')';
+dic[')'] = '(';
+dic['['] = ']';
+dic[']'] = '[';
+dic['{'] = '}';
+dic['}'] = '{';
 
-    return minLenght;
-}
+var isValid = function (s) {
+    for (let i = 0; i < s.length; i += 2) {
+        if (s.includes()) {
 
-const longestCommonPrefix = (strs) => {
-    let wordCounter = 0;
-    let letterCounter = 0;
-    let letter = "";
-    let minWordLength = findMinSizeWord(strs);
-    let numberOfRepetition = minWordLength * strs.length;
-    let myStringLength = strs.length;
-
-    let longestCommon = "";
-
-    if (myStringLength === 1)
-        return strs[0]
-    else {
-        for (let i = 1; i <= numberOfRepetition; i++) {
-            //if given string length is 1 we don't need to go into for loop
-            if (longestCommon.length === minWordLength)
-                break;
-
-            //setting to letter which compare 
-            //and also
-            //reset to wordCounter which use for determining to which word is comparing right now
-            if (wordCounter === 0) {
-                letter = strs[wordCounter][letterCounter]
-                wordCounter++;
-            }
-
-            let currentLetter = strs[wordCounter][letterCounter]
-            if (letter === currentLetter) {
-                if (wordCounter === myStringLength - 1)
-                    longestCommon += letter;
-            }
-            else
-                break;
-
-            wordCounter++;
-
-            //checking of is wordCounter value higher than lengthOfString
-            if (wordCounter === myStringLength) {
-                letterCounter++;
-                wordCounter = 0;
-            }
         }
-        return longestCommon
+        else if (s[i] !== dic[s[i + 1]])
+            return false;
     }
-}
+    return true;
+};
 
-console.log(longestCommonPrefix(["flower", "flow", "flight"]))
+console.log(isValid("{[]}"))
