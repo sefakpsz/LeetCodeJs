965c0b76c1d0d13d240bdceb1fca2f7cabc06f8d
sefa kapısız <sefa.kapisiz@coino.com>
2023-06-05 07:31:18 +0300

    upgrade with comments

--- app.ts
+++ app.ts
@@ -15,18 +15,18 @@ brackets['}'] = '{'
 
 
 var isValid = (parantheses) => {
-    // if this is not plural then definitely one of paranthes has not a peer
-    // if (parantheses.length % 2 !== 0)
-    //     return false
+    //if this is not plural then definitely one of paranthes has not a peer
+    if (parantheses.length % 2 !== 0)
+        return false
 
-    // if (parantheses.length === 2)
-    //     if (brackets[parantheses[0]] !== parantheses[1])
-    //         return false
-    //     else {
-    //         if (closings.includes(parantheses[0]))
-    //             return false
-    //         return true
-    //     }
+    if (parantheses.length === 2)
+        if (brackets[parantheses[0]] !== parantheses[1])
+            return false
+        else {
+            if (closings.includes(parantheses[0]))
+                return false
+            return true
+        }
 
     for (let i = 0; i < parantheses.length; i++) {
         if (i + 1 === parantheses.length)
@@ -42,15 +42,10 @@ var isValid = (parantheses) => {
 };
 
 /*
-Suggestions from ChatGPT
+Bad things about this algorithm are using useless validations and it makes code less performance and 
+also array manipulation is expensive also using substring method means that creating new string and this also expensive operation
 
-1.Remove unnecessary checks: The first part of your code checks if the length of the input string is 2 and performs some validations. 
-You can remove this check since it's already covered in the general logic of the function.
-
-2.Avoid string manipulation: In your current implementation, you modify the parantheses string by removing matching pairs of parentheses 
-using the substring function. String manipulation operations like this create new strings, 
-which can be expensive in terms of memory and runtime. Instead of modifying the string, 
-you can use a stack data structure to keep track of the open parentheses.
+Instead of array or string manipulation stack data structure can be used in here and remove unnecessary validation checks 👌
 */
 
 console.log(isValid("(([]){})"))
