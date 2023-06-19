055485ab060086b45fc348de0f8a7c7620e92834
sefa kapısız <sefa.kapisiz@coino.com>
2023-06-19 15:01:45 +0300

    done lastly

--- app.ts
+++ app.ts
@@ -7,52 +7,18 @@ class ListNode {
     }
 }
 
-const addTwoNumbers = (l1: ListNode | null, l2: ListNode | null) => {
-    const ints = []
-    const ints2 = []
-    while (l1) {
-        ints.push(l1.val)
-        l1 = l1.next
+const push = (l: ListNode, nextValue: number, next: boolean): ListNode => {
+    if (next) {
+        l.val = nextValue
+        return l
     }
-    ints.forEach(i => { console.log(i) })
-
-    let ten = 1
-
-    let sum = 0
-    for (let index = 0;index < ints.length;index++) {
-        ints[index] *= ten
-        sum += ints[index]
-        ten = ten * 10
-    }
-
-    while (l2) {
-        ints2.push(l2.val)
-        l2 = l2.next
-    }
-    ints2.forEach(i => { console.log(i) })
-
-    ten = 1
-    for (let index = 0;index < ints2.length;index++) {
-        ints2[index] *= ten
-        sum += ints2[index]
-        ten = ten * 10
-    }
-
-    const sumString = sum.toString().split('')
-
-    let a = ""
-    sumString.reverse().forEach(s => a += s)
-    console.log(a)
-
-    let newNode = new ListNode()
-    for (let index = 0;index < sumString.length;index++) {
-        newNode = push(newNode, parseInt(sumString[index]), index === 0)
-    }
-
+    const newNode = new ListNode()
+    newNode.val = nextValue
+    newNode.next = l
     return newNode
 }
 
-const addTwoNumbers2 = (l1: ListNode | null, l2: ListNode | null) => {
+const addTwoNumbers = (l1: ListNode | null, l2: ListNode | null) => {
     const ints = []
     const ints2 = []
     while (l1) {
@@ -138,38 +104,12 @@ const addTwoNumbers2 = (l1: ListNode | null, l2: ListNode | null) => {
     return newNode
 }
 
-const push = (l: ListNode, nextValue: number, next: boolean): ListNode => {
-    if (next) {
-        l.val = nextValue
-        return l
-    }
-    const newNode = new ListNode()
-    newNode.val = nextValue
-    newNode.next = l
-    return newNode
-}
-
-// const addition = (bit: number, ints: [], ints2: [], length: number, newNode: ListNode, i: number) => {
-//     if (bit >= 10) {
-//         newNode = push(newNode, bit % 10, i === 0)
-//         if (ints[i + 1]) {
-//             ints[i + 1]++
-//             length++
-//         }
-//         else {
-//             ints2[i + 1]++
-//             length++
-//         }
-//     } else
-//         newNode = push(newNode, bit, i === 0)
-// }
-
-addTwoNumbers2(
+addTwoNumbers(
     new ListNode(2, new ListNode(4, new ListNode(9))),
     new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(9))))
 )
 
-addTwoNumbers2(
+addTwoNumbers(
     new ListNode(1, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1)))))))))))))))))))))),
     new ListNode(5, new ListNode(6, new ListNode(4)))
 )
