552bffc6aa36002fc387144f5ed8a44f0e74c12b
sefa kapısız <sefa.kapisiz@coino.com>
2023-06-21 22:52:08 +0300

    progress...

--- app.ts
+++ app.ts
@@ -1,16 +1,43 @@
-interface IResult {
-    sub: string,
-    repetition: Number
+function lengthOfLongestSubstring(s: string): number {
+    // let subPast = []
+    // let subCurrent = []
+
+    // for (let letter of s) {
+    //     if (subCurrent.indexOf(letter) !== -1) {
+    //         if (subPast.length < subCurrent.length) {
+    //             subPast = []
+    //             subCurrent.map(s => subPast.push(s))
+    //         }
+
+    //         subCurrent = []
+    //         subCurrent.push(letter)
+    //     }
+    //     else {
+    //         subCurrent.push(letter)
+    //         subPast.push(letter)
+    //     }
+    // }
+
+    // return subPast.length
+
+    let sub = []
+    let subLength = 0
+
+    for (let letter of s) {
+        //that means if sub includes "letter"
+        if (sub.indexOf(letter) !== -1) {
+            if (subLength < sub.length) {
+                subLength = sub.length
+            }
+
+            sub = []
+        }
+        else {
+            sub.push(letter)
+        }
+    }
+
+    return subLength
 }
 
-let dic: { [index: number]: IResult } = {}
-
-dic[0] = { sub: "wke", repetition: 1 }
-
-console.log(dic[0])
-
-
-interface Dictionary<Number> {
-    [sub: string]: Number
-}
-
+console.log(lengthOfLongestSubstring("abcabcab"))
