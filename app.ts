1f5dde45b96789ffa850e01e231eb737386b1ae6
sefa kapısız <sefa.kapisiz@coino.com>
2023-08-21 18:14:16 +0300

    feat:progress..

--- app.ts
+++ app.ts
@@ -1,41 +1,19 @@
-// const longestPalindrome = (s: string): string => {
-//     let tempArray = []
-//     let palindromics = []
-//     let reverseVersionOfTemp = []
-//     let queue = 0
-//     //it needs to run s.length * s.length times and each s.length time need to start from one more than last value of i
+interface Palindrome {
+    word: string
+    length: number
+}
 
-//     for (let i = queue; i < s.length; i++) {
-//         tempArray.push(s[i])
+let palindromeWords: Array<Palindrome>
 
-//         if (tempArray.length > 1) {
-//             reverseVersionOfTemp = tempArray.slice().reverse()
+function longestPalindrome(s: string): string {
+    let temp = ''
+    for (const letter of s) {
+        temp += letter
+    }
 
-//             if (tempArray.toString() === reverseVersionOfTemp.toString()) {
-//                 palindromics.push(tempArray.toString())
-//             }
-//         }
+    return ''
+}
 
-//         reverseVersionOfTemp = []
-
-//         if (i === s.length - 1) {
-//             queue++
-//             if (queue === s.length) break
-//             i = 0
-//             tempArray = []
-//         }
-//     }
-
-//     palindromics.forEach((s) => console.log(s))
-//     return 'null'
-// }
-
-//longestPalindrome('ababbaba')
-//aba
-//ababbaba
-//bab
-//babbab
-//abba
-//bb
-//bab
-//aba
+console.log(longestPalindrome('babad')) // --> aba
+console.log(longestPalindrome('cbbd')) // --> bb
+console.log(longestPalindrome('acababaccdaba')) // --> cababac
